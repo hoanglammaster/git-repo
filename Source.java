2cf53a7255dc56fc9dd7eeb45a6af23e07774cd4
Hoang Lam <lam.hoangvan@apuscorp.com>
2023-06-03 09:34:27 +0700

    First commit

---
+++ Source.java
@@ -0,0 +1,8 @@
+public class Source {
+
+    public static void main(String[] args) {
+        System.out.println("This commit from master");
+        System.out.println("This line in commit 2");
+        System.out.println("This line in commit 3");
+    }
+}
